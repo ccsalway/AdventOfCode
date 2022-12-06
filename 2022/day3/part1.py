4142e8c6158a304d34784b7dc7e9e60b5662bff4
Christian Salway <christian@salway.im>
2022-12-06 09:49:19 +0000

    day3

---
+++ 2022/day3/part1.py
@@ -0,0 +1,16 @@
+total = 0
+
+with open('input.txt') as f:
+    for line in f.read().splitlines():
+        n = len(line)
+        h = n // 2  # assuming all lines are perfectly divisible by 2
+        f = set(line[0:h])
+        s = set(line[h:n])
+        i = [ord(x) for x in f & s][0]
+        if i >= 97:  # a-z
+            total += i - 97 + 1
+        elif i >= 65:  # A-Z
+            total += i - 65 + 27
+
+assert total == 7821
+print(total)
---
+++ 2022/day3/part2.py
@@ -0,0 +1,17 @@
+total = 0
+
+with open('input.txt') as f:
+    lines = []
+    for index, line in enumerate(f.read().splitlines()):
+        lines.append(set(line))
+        if (index + 2) % 3 == 1:
+            a, b, c = lines
+            i = [ord(x) for x in a & b & c][0]
+            if i >= 97:  # a-z
+                total += i - 97 + 1
+            elif i >= 65:  # A-Z
+                total += i - 65 + 27
+            lines = []
+
+assert total == 2752
+print(total)
