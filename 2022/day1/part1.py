7c46aaf76548f08817d8ebd0520d3c78809a4901
Christian Salway <christian@salway.im>
2022-12-06 09:48:36 +0000

    day1

---
+++ 2022/day1/part1.py
@@ -0,0 +1,16 @@
+totals = []
+
+with open('input.txt') as f:
+    subtotal = 0
+    for line in f.read().splitlines():
+        # dont rely on newline being \n
+        try:
+            subtotal += int(line)
+        except ValueError:
+            # will cause an exception on each blank line
+            totals.append(subtotal)
+            subtotal = 0
+
+result = max(totals)
+assert result == 71300
+print(result)
---
+++ 2022/day1/part2.py
@@ -0,0 +1,18 @@
+totals = []
+
+with open('input.txt') as f:
+    subtotal = 0
+    for line in f.read().splitlines():
+        # dont rely on newline being \n
+        try:
+            subtotal += int(line)
+        except ValueError:
+            # will cause an exception on each blank line
+            totals.append(subtotal)
+            subtotal = 0
+
+totals.sort(reverse=True)
+
+result = sum(totals[0:3])
+assert result == 209691
+print(result)
