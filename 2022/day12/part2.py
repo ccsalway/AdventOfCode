90cf80a81b8f5d5cb619e1de04e7e669de279af7
Christian Salway <christian@salway.im>
2022-12-12 15:00:16 +0000

    day12

--- 2022/day12/part2.py
+++ 2022/day12/part2.py
@@ -1,36 +1,51 @@
-import math
-
-monkeys = []
-inspect = []
-
 with open('input.txt') as f:
-    for m in f.read().split('\n\n'):
-        lines = m.splitlines()
-        items = [i.strip() for i in lines[1].split(':')[1].split(',')]
-        op = lines[2].split('=')[1].strip()
-        test = int(lines[3].split(' ')[-1])
-        true = int(lines[4].split(' ')[-1])
-        false = int(lines[5].split(' ')[-1])
-        monkey = [items, op, test, true, false]
-        monkeys.append(monkey)
-        inspect.append(0)
-        # print(monkey)
+    rows = f.read().splitlines()
 
-lcm = math.lcm(*[n[2] for n in monkeys])
+m, start, e = [], [], ()
+for y in range(len(rows)):
+    m.append([])
+    for x in range(len(rows[y])):
+        col = rows[y][x]
+        v = ord(col)
+        if col in ['b', 'S']:  # less b's to check than a's
+            start.append((y, x))
+            if col == 'S':
+                v = ord('a')
+        elif col == 'E':
+            v = ord('z') + 1
+            e = y, x
+        m[-1].append(v)
 
-for r in range(10000):
-    for n in range(len(monkeys)):
-        m = monkeys[n]
-        inspect[n] += len(m[0])
-        for i in m[0]:
-            r = eval(m[1].replace('old', i)) % lcm
-            if r % m[2] == 0:
-                monkeys[m[3]][0].append(str(r))
-            else:
-                monkeys[m[4]][0].append(str(r))
-        m[0] = []
+steps = []
+for s in start:
+    p = []
+    for y in range(len(rows)):
+        p.append([])
+        for x in range(len(rows[y])):
+            p[-1].append(0)
+    p[s[0]][s[1]] = 1  # starting position
+    k = 0
+    while p[e[0]][e[1]] == 0:
+        k += 1
+        for y in range(len(rows)):
+            for x in range(len(rows[y])):
+                if p[y][x] == k:
+                    n = m[y][x]
+                    # left
+                    if x > 0 and p[y][x - 1] == 0 and m[y][x - 1] <= n + 1:
+                        p[y][x - 1] = k + 1
+                    # up
+                    if y > 0 and p[y - 1][x] == 0 and m[y - 1][x] <= n + 1:
+                        p[y - 1][x] = k + 1
+                    # right
+                    if x < len(rows[y]) - 1 and p[y][x + 1] == 0 and m[y][x + 1] <= n + 1:
+                        p[y][x + 1] = k + 1
+                    # down
+                    if y < len(rows) - 1 and p[y + 1][x] == 0 and m[y + 1][x] <= n + 1:
+                        p[y + 1][x] = k + 1
+    steps.append(k + 1)  # add 1 as we started from 'b'
 
-result = math.prod(sorted(inspect)[-2:])
-print(result)
+shortest = sorted(steps)[0]
+print(shortest)
 
-assert result == 13237873355
+assert shortest == 321
